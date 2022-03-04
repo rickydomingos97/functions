54db085b18b317521318c84a72eb2ae48c28355a
rickydomingos97 <iricardo97@protonmail.com>
2022-03-04 03:45:30 -0300

    pratika function 1

--- function.js
+++ function.js
@@ -4,12 +4,17 @@
 
 // parametros da funcao
 const sum = function(number1, number2) {
-    console.log(number1 + number2);
+    console.log(number1 * number2);
 }
 
-let number1 = 20
-let number2 = 11
+//let number1 = 20
+//let number2 = 11
+//sum(number1, number2)
+//console.log(`o numero 1 é ${number1}`)
+//console.log(`O numero 2 é ${number2}`)
+//console.log(`a soma é ${number1, number2}`)
+
+let number1 = 2
+let number2 = 4
 sum(number1, number2)
-console.log(`o numero 1 é ${number1}`)
-console.log(`O numero 2 é ${number2}`)
-console.log(`a soma é ${number1, number2}`)
+console.log(sum)
