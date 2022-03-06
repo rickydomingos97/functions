2c9a6f8203f4885e5787057bef9b479da48e3b8b
rickydomingos <ricardodomingos97@gmail.com>
2022-03-06 13:31:20 -0300

    omelete

--- function.js
+++ function.js
@@ -1,9 +1,16 @@
 // funcao eh um liquidificador
 
 function fazerSuco(fruta1, fruta2) {
-    return fruta1 + fruta2
+    return 'suco de ' + fruta1 + fruta2
 }
 
 const copo = fazerSuco('banana', 'maça')
 
 console.log(copo)
+
+function fazerOmelete(ingrediente1, ingrediente2) {
+    return ingrediente1 + ingrediente2
+}
+
+const panela = fazerOmelete('ovo', 'ovos')
+console.log(panela)
