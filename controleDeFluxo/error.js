46e524af08b2eb8c1db7d8686af71b5efce8ea5e
rickydomingos <ricardodomingos97@gmail.com>
2022-04-14 09:08:03 -0300

    pratika dia 14april22

--- controleDeFluxo/error.js
+++ controleDeFluxo/error.js
@@ -1,17 +1,27 @@
-// throw
-
-// function sayMyName(name)
-// try... catch
-
-function Pessoa(nome) {
-    this.nome = nome
-    this.andar = ()=>{
-        return this.nome + ' esta andando'
+//throw = throw new Error("Nome é necessario")
+function sayMyName(name = '') {
+    if (name === '') {
+        throw 'Nome eh obrigatorio'
     }
-    this.comer = ()=>{
-        return this.nome + ' esta comendo banana'
-    }
+    console.log('depois do erro')
 }
+// try... catch
+try {
+    sayMyName('ricky')
+} catch(e) {
+    console.log(e)
+}
+console.log('depois do try/ catch')
 
-const joao = new Pessoa('Joao')
-console.log(joao.comer())
+function fecharVenda(venda) {
+    if(venda === '') {
+        throw 'Nenhum produto adicionado!'
+    }
+    console.log('Forma de pagamento')
+}
+try {
+    fecharVenda('')
+}catch(e) {
+    console.log(e)
+}
+console.log('venda concluida')
