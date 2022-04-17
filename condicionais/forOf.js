a867d879e7bef3f6bece740eb483b2ce6f9876a2
rickydomingos <ricardodomingos97@gmail.com>
2022-04-17 11:18:55 -0300

    pratika for of

--- condicionais/forOf.js
+++ condicionais/forOf.js
@@ -1,13 +1,6 @@
 // for ... of
 // vai criar um lop atraves de uma variavel previamente existente
-let nome = 'Mayk'
-let names = ['Joao', 'Paulo', 'Pedro']
-
-/**
- * for(let name of names) {
-    console.log(name)
-}
- */
+let nome = 'Ricardo'
 
 for(let char of nome) {
     console.log(char)
