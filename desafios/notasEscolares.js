5ac00b371655418c3dc26cbd21a146d853509daa
rickydomingos <ricardodomingos97@gmail.com>
2022-04-19 13:34:19 -0300

    solucao minha e da aula

--- desafios/notasEscolares.js
+++ desafios/notasEscolares.js
@@ -11,8 +11,9 @@
  * 
  */
 
-/**Solucao 1 */
-let nota = 91
+/**Solucao 1
+ * 
+ * let nota = 91
 
 if(nota > 90) {
     console.log('A')
@@ -25,3 +26,28 @@ if(nota > 90) {
 }else {
     console.log('F')
 }
+*/
+let score = 88
+let scoreA = score >= 90 && score <=100
+let scoreB = score >= 80 && score <= 89
+let scoreC = score >= 70 && score <= 79
+let scoreD = score >= 60 && score <= 69
+let scoreF = score < 60 && score >= 0
+
+let scoreFinal
+
+if (scoreA) {
+    scoreFinal = "A"
+} else if ( scoreB ) {
+    scoreFinal = "B"
+} else if (scoreC) {
+    scoreFinal = "C"
+} else if (scoreD) {
+    scoreFinal = "D"
+} else if(scoreF) {
+    scoreFinal = "F"
+}else if (scoreFinal) {
+    scoreFinal = "Nota invalida"
+}
+console.log('A sua nota foi: ' + score)
+console.log('O seu nivel foi: ' + scoreFinal)
