d54700bb6c5f6aff25db4f0946dca62bce63e6c7
rickydomingos <ricardodomingos97@gmail.com>
2022-04-20 08:57:28 -0300

    refactor to function getNota

--- desafios/notasEscolares.js
+++ desafios/notasEscolares.js
@@ -54,19 +54,24 @@ console.log('A sua nota foi: ' + score)
 console.log(scoreFinal)
 */
 
-let nota = -9
+//let nota = -9
 
-if(nota > 90 && nota <= 100) {
-    console.log('A')
-}else if(nota <= 90 && nota >= 80) {
-    console.log('B')
-}else if(nota >= 70 && nota <= 79) {
-    console.log('C')
-}else if (nota <= 69 && nota >= 60) {
-    console.log('D')
-}else if (nota < 60 && nota >= 0){
-    console.log('F')
-}else {
-    console.log("opcao invalida")
+function getNota(nota) {
+    if(nota > 90 && nota <= 100) {
+        console.log('A')
+    }else if(nota <= 90 && nota >= 80) {
+        console.log('B')
+    }else if(nota >= 70 && nota <= 79) {
+        console.log('C')
+    }else if (nota <= 69 && nota >= 60) {
+        console.log('D')
+    }else if (nota < 60 && nota >= 0){
+        console.log('F')
+    }else {
+        console.log("opcao invalida! tente novamente")
+    }
+    return nota
 }
-console.log("O seu score foi de : " + nota)
+
+getNota(104)
+getNota(66)
